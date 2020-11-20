a1c4fd143bed38dc0ce3b23901f10b422038b7a7
marcosdgl <marcos.davidovich@globallogic.com>
2020-11-20 17:14:22 -0300

    one more tesst

--- .github/scripts/decrypt_secret.sh
+++ .github/scripts/decrypt_secret.sh
@@ -4,5 +4,5 @@ ls
 # Decrypt the file
 # --batch to prevent interactive command
 # --yes to assume "yes" for questions
-gpg --quiet --batch --yes --decrypt --pinentry-mode loopback --passphrase="mandarina" \
+gpg --quiet --batch --yes --decrypt --pinentry-mode loopback --passphrase="$LARGE_SECRET_PASSPHRASE" \
 --output ../../src/firebaseAuth.json firebaseAuth.json.gpg
