902e7873d7d1d073e9c645d530f4d2a9ac503f64
marcosdgl <marcos.davidovich@globallogic.com>
2020-11-21 21:20:49 -0300

    functions approach

---
+++ functions/services/userService.js
@@ -0,0 +1,17 @@
+var log = require('loglevel');
+var firebase = require("firebase/app");
+require('firebase/auth');
+require('firebase/database');
+
+const writeUserData = function (user) {
+    const timestamp = Date.now();
+    firebase.database().ref('users/').push({
+        username: user.name,
+        email: user.email,
+    });
+    log.info(`Data sent to FireBase correctly at ${timestamp}`);
+}
+
+module.exports = {
+    writeUserData
+}
---
+++ functions/tests/userTest.js
@@ -0,0 +1,43 @@
+const chai = require('chai');
+const chaiHttp = require('chai-http');
+const app = require('../main');
+chai.use(chaiHttp);
+chai.should();
+
+describe("Users: ", () => {
+    describe("GET /", () => {
+        it("should get all the users", (done) => {
+            chai.request(app)
+                .get('/getUsers')
+                .end((err, res) => {
+                    res.should.have.status(200);
+                    res.text.should.match(/([{\w":.,@}])+/g)
+                    done();
+                });
+        });
+        
+        it("should get a user", (done) => {
+            chai.request(app)
+                .get('/getUserById/-M-j81ojzvtUFRgtIH5d')
+                .end((err, res) => {
+                    res.should.have.status(200);
+                    res.text.should.includes("username")  
+                    res.text.should.includes("email")  
+                    done();
+                });
+        });
+
+    });
+    describe("POST /", () => {
+        it("should create a user", (done) => {
+            const user = { name: "testAut", email: "test@aut.com" }
+            chai.request(app)
+                .post('/createUser')
+                .send(user)
+                .end((err, res) => {
+                    res.should.have.status(201);
+                    done();
+                });
+        })
+    })
+});
